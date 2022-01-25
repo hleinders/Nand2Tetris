c205066930f83f87dc0073d3c1a8be024336f139
Harald Leinders <harald.leinders@denkwerk.com>
2022-01-25 16:04:15 +0100

    remediate linter warning

--- Hack/hackAs/stdinit.go
+++ Hack/hackAs/stdinit.go
@@ -22,7 +22,8 @@ const (
 var bold = color.New(color.Bold).SprintFunc()
 var red = color.New(color.FgRed).SprintFunc()
 var yellow = color.New(color.FgYellow).SprintFunc()
-var green = color.New(color.FgGreen).SprintFunc()
+
+// var green = color.New(color.FgGreen).SprintFunc()
 
 // Return values
 const (
